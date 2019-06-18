ac0742893f622d650cfecf32ed68f360f22c0514
Jan Kunzmann <jan.kunzmann@erasys.de>
2019-06-18 14:49:24 +0200

    add support for optional value array

--- lib/ical_filter_proxy/filter_rule.rb
+++ lib/ical_filter_proxy/filter_rule.rb
@@ -1,31 +1,35 @@
 module IcalFilterProxy
   class FilterRule
 
-    attr_accessor :field, :operator, :value, :negation
+    attr_accessor :field, :operator, :values, :negation
 
-    def initialize(field, operator, value)
+    def initialize(field, operator, values)
       self.field = field
       operator =~ /^(not-)?(\w+)/
       self.negation = !$1.nil?
       self.operator = $2
-      self.value = value
+      self.values = values
     end
 
     def match_event?(filterable_event)
       event_data = filterable_event.send(field.to_sym)
-      negation ^ evaluate(event_data)
+      negation ^ evaluate(event_data, values)
     end
 
     private
 
-    def evaluate(event_data)
-      case operator
-      when 'equals'
-        event_data == value
-      when 'startswith'
-        event_data.start_with?(value)
+    def evaluate(event_data, value)
+      if value.is_a? Array
+        value.reduce(false) { |r, v| r |= evaluate(event_data, v) }
       else
-        false
+        case operator
+        when 'equals'
+          event_data == value
+        when 'startswith'
+          event_data.start_with?(value)
+        else
+          false
+        end
       end
     end
   end
