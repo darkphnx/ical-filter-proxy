069225191f9b12bd86806fd4480272276024608f
Dan Wentworth <dan@atechmedia.com>
2022-12-20 19:14:15 +0000

    calendars not filters

--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@ require 'rack'
 require_relative 'lib/ical_filter_proxy'
 require_relative 'lib/ical_filter_proxy/servers/rack_app'
 
-filters = IcalFilterProxy.filters
-app = IcalFilterProxy::Servers::RackApp.new(filters)
+calendars = IcalFilterProxy.calendars
+app = IcalFilterProxy::Servers::RackApp.new(calendars)
 
 run app
--- lambda.rb
+++ lambda.rb
@@ -4,8 +4,8 @@ require_relative 'lib/ical_filter_proxy/servers/lambda_app'
 # Entry point for AWS Lambda
 
 def handle(event:, context:)
-  filters = IcalFilterProxy.filters
-  app = IcalFilterProxy::Servers::LambdaApp.new(filters)
+  calendars = IcalFilterProxy.calendars
+  app = IcalFilterProxy::Servers::LambdaApp.new(calendars)
 
   app.call(event)
 end
